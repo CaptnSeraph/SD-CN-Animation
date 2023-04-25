0fd0008c3eb3a99900297f24b4607f94fa7463d8
theseraphim <s3raph1m@gmail.com>
2023-04-25 02:26:56 +0100

    various changes for background removal

--- compute_flow.py
+++ compute_flow.py
@@ -1,93 +1,73 @@
 import cv2
+import base64
 import numpy as np
 from tqdm import tqdm
 import os
+
+from flow_utils import RAFT_estimate_flow
 import h5py
+from rembg import remove, new_session
+from PIL import Image
 import argparse
 
-from rembg import remove
-from PIL import Image
-
-def remove_background_video(input_video, output_video):
-    input_video_cap = cv2.VideoCapture(input_video)
-    width = int(input_video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(input_video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(input_video_cap.get(cv2.CAP_PROP_FPS))
-    codec = int(input_video_cap.get(cv2.CAP_PROP_FOURCC))
-    total_frames = int(input_video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    output_video_writer = cv2.VideoWriter(output_video, codec, fps, (width, height), isColor=True)
-
-    progress_bar = tqdm(range(total_frames), desc="Processing frames", ncols=100)
-
-    for _ in progress_bar:
-        ret, frame = input_video_cap.read()
-        if not ret:
-            break
-
-        pil_frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        output_frame = remove(pil_frame, session=session)
-        output_frame = cv2.cvtColor(np.array(output_frame), cv2.COLOR_RGB2BGR)
-
-        output_video_writer.write(output_frame)
-
-    input_video_cap.release()
-    output_video_writer.release()
-
-
 def main(args):
-    from rembg import new_session
-
+  W, H = args.width, args.height
+  # create session for rembg
+  session = None
+  if args.remove_background:
     session = new_session(gpu=True)
+  # Open the input video file
+  input_video = cv2.VideoCapture(args.input_video)
 
+  # Get useful info from the source video
+  fps = int(input_video.get(cv2.CAP_PROP_FPS))
+  total_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    W, H = args.width, args.height
+  prev_frame = None
 
-    if args.remove_background:
-      pil_cur_frame = Image.fromarray(cv2.cvtColor(cur_frame, cv2.COLOR_BGR2RGB))
-      removed_background_frame = remove(pil_cur_frame, session=session)
-      cur_frame = cv2.cvtColor(np.array(removed_background_frame), cv2.COLOR_RGB2BGR)
+  # create an empty HDF5 file
+  with h5py.File(args.output_file, 'w') as f: pass
 
-    input_video = cv2.VideoCapture(args.input_video)
-    fps = int(input_video.get(cv2.CAP_PROP_FPS))
-    total_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+  # open the file for writing a flow maps into it
+  with h5py.File(args.output_file, 'a') as f:
+    flow_maps = f.create_dataset('flow_maps', shape=(0, 2, H, W, 2), maxshape=(None, 2, H, W, 2), dtype=np.float16) 
 
-    prev_frame = None
+    for ind in tqdm(range(total_frames)):
+      # Read the next frame from the input video
+      if not input_video.isOpened(): break
+      ret, cur_frame = input_video.read()
+      if not ret: break
 
-    with h5py.File(args.output_file, 'w') as f: pass
+      cur_frame = cv2.resize(cur_frame, (W, H))
+      #if remove background flag is set, remove background using rembg
+      if args.remove_background:
+        pil_cur_frame = Image.fromarray(cv2.cvtColor(cur_frame, cv2.COLOR_BGR2RGB))
+        removed_background_frame = remove(pil_cur_frame, session=session)
+        cur_frame = cv2.cvtColor(np.array(removed_background_frame, cv2.COLOR_RGB2BGR))
 
-    with h5py.File(args.output_file, 'a') as f:
-        flow_maps = f.create_dataset('flow_maps', shape=(0, 2, H, W, 2), maxshape=(None, 2, H, W, 2), dtype=np.float16) 
+      if prev_frame is not None:
+        next_flow, prev_flow, occlusion_mask = RAFT_estimate_flow(prev_frame, cur_frame)
 
-        for ind in tqdm(range(total_frames)):
-            if not input_video.isOpened(): break
-            ret, cur_frame = input_video.read()
-            if not ret: break
+        # write data into a file
+        flow_maps.resize(ind, axis=0)
+        flow_maps[ind-1, 0] = next_flow
+        flow_maps[ind-1, 1] = prev_flow
 
-            cur_frame = cv2.resize(cur_frame, (W, H))
+        occlusion_mask = np.clip(occlusion_mask * 0.2 * 255, 0, 255).astype(np.uint8)
 
-            if prev_frame is not None:
-                next_flow, prev_flow, occlusion_mask = RAFT_estimate_flow(prev_frame, cur_frame)
+        if args.visualize:
+          # show the last written frame - useful to catch any issue with the process
+          img_show = cv2.hconcat([cur_frame, occlusion_mask])
+          cv2.imshow('Out img', img_show)
+          if cv2.waitKey(1) & 0xFF == ord('q'): exit() # press Q to close the script while processing
 
-                flow_maps.resize(ind, axis=0)
-                flow_maps[ind-1, 0] = next_flow
-                flow_maps[ind-1, 1] = prev_flow
+      prev_frame = cur_frame.copy()
 
-                occlusion_mask = np.clip(occlusion_mask * 0.2 * 255, 0, 255).astype(np.uint8)
+  # Release the input and output video files
+  input_video.release()
 
-                if args.visualize:
-                    img_show = cv2.hconcat([cur_frame, occlusion_mask])
-                    cv2.imshow('Out img', img_show)
-                    if cv2.waitKey(1) & 0xFF == ord('q'): exit()
-
-            prev_frame = cur_frame.copy()
-
-    input_video.release()
-
-    if args.visualize: cv2.destroyAllWindows()
-
-    if args.remove_background:
-        os.remove(temp_video_file)
+  # Close all windows
+  if args.visualize: cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
